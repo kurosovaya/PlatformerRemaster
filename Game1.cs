c49d6efd06ac2a34dba2987a499b858ce433f6ec
na.shornikov <nonickk9@gmail.com>
2021-12-28 00:00:29 +0300

    init

---
+++ Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlatformerRemaster
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D background_1;
+        Texture2D character_img;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+            _graphics.IsFullScreen = true;
+        }
+
+        protected override void Initialize()
+        {
+            // IDGF
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            background_1 = Content.Load<Texture2D>("trees_palms_jungle_127762_3840x2096");
+            character_img = Content.Load<Texture2D>("cover-256");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background_1, new Rectangle(0, 0, 1920, 1080), Color.White);
+            _spriteBatch.Draw(character_img, new Rectangle(0, 0, 64, 64), Color.White);
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
